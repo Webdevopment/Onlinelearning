bda285018926aadf52918f28c992585e2b359f23
Jaliya Madusnka <rmjmbandara@students.nsbm.ac.lk>
2021-09-09 19:07:40 +0530

    get name from post method

---
+++ student.php
@@ -0,0 +1,9 @@
+<?php
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$Birthdate = $_POST['Birthdate'];
+$gender = $_POST['gender'];
+
+?>
