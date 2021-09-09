06a8e3eddedb64813215b2d192e8fcee30f3164a
Jaliya Madusnka <rmjmbandara@students.nsbm.ac.lk>
2021-09-09 19:09:47 +0530

    insert data into database

--- student.php
+++ student.php
@@ -10,5 +10,15 @@ $conn = new mysqli('localhost','root','','visionnew');
 	if($conn->connect_error){
 		die('Connection Failed :' .$conn->connect_error);
 	}
+    else{
+		$stmt = $conn->prepare("insert into student(firstname,lastname,email,password,Birthdate,gender)
+			values(?,?,?,?,?,?)");
+		$stmt->bind_param("ssssss",$firstname, $lastname, $email,$password, $Birthdate, $gender);
+		$stmt->execute();
+		echo "<script> window.location.assign('#'); </script>";
+		$stmt->close();
+		$conn->close();
+	}
+    
 
 ?>
