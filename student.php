0c08c5560a7cefd8282a0262066e2ab7f3a1c7f6
Jaliya Madusnka <rmjmbandara@students.nsbm.ac.lk>
2021-09-09 19:08:32 +0530

    connect with database

--- student.php
+++ student.php
@@ -6,4 +6,9 @@ $password = $_POST['password'];
 $Birthdate = $_POST['Birthdate'];
 $gender = $_POST['gender'];
 
+$conn = new mysqli('localhost','root','','visionnew');
+	if($conn->connect_error){
+		die('Connection Failed :' .$conn->connect_error);
+	}
+
 ?>
