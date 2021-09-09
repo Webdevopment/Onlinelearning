010b34c62780c54e59e2a9776be5e338d363dab0
Jaliya Madusnka <rmjmbandara@students.nsbm.ac.lk>
2021-09-09 19:51:20 +0530

    get data from post method

---
+++ teacher.php
@@ -0,0 +1,17 @@
+<?php
+
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$contact = $_POST['contact'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$birthdate = $_POST['birthdate'];
+$gender = $_POST['gender'];
+$discibe = $_POST['discibe'];
+$qualification = $_POST['qualification'];
+
+
+
+
+
+?>
